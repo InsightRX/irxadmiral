82264da27cd5ce256bddaf8be8c1b585d55c44dc
Kara Woo <kara@insight-rx.com>
2023-09-12 20:21:13 -0700

    Add tests for functions that read data

---
+++ tests/testthat/helper.R
@@ -0,0 +1,10 @@
+# Create a temporary directory and put a couple .xpt files in it
+local_create_data_dir <- function(dir = fs::file_temp(), env = parent.frame()) {
+  fs::dir_create(dir)
+  withr::defer(fs::dir_delete(dir), envir = env)
+
+  fs::file_create(fs::path(dir, "mtcars.xpt"))
+  fs::file_create(fs::path(dir, "trees.xpt"))
+  haven::write_xpt(mtcars, path = fs::path(dir, "mtcars.xpt"))
+  haven::write_xpt(trees, path = fs::path(dir, "trees.xpt"))
+}
